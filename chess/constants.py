3da64d84f240d7d0d088d1705f7542f979cc6773
Ivan <iv.panico@gmail.com>
2023-12-25 19:11:27 +0100

    setting up pre-commit

---
+++ chess/constants.py
@@ -0,0 +1,2 @@
+USERNAME = "FollowK"
+CHESS_API_URI = f"https://api.chess.com/pub/player/{USERNAME}/games"
---
+++ chess/main.py
@@ -0,0 +1,10 @@
+from constants import CHESS_API_URI
+
+
+def get_games():
+    print(CHESS_API_URI)
+    pass
+
+
+if __name__ == "__main__":
+    get_games()
